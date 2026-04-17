a2e2ee3eb6df4eb677c93f69de442217487acdcb
paminalessle <paminalessle@gmail.com>
2026-04-17 19:08:25 +0000

    program hung up

--- courses/coding-in-C++/Solutions/Lab_1.cpp
+++ courses/coding-in-C++/Solutions/Lab_1.cpp
@@ -18,16 +18,12 @@ namespace _validation_ {
         if (65 > age >= 18){
             return true;
         }
-
     }
 
     bool isSenior(std::int_least8_t age) {
         if(age >= 65){
             return true;
-        
         }
-    std::cout << "You are " << age << "years old." << std::endl;
-    std::cout << "That means you are a senior citizen" << std::endl;
     }
 
 }
@@ -35,14 +31,15 @@ namespace _validation_ {
 int main(){
     std::cout << "Please enter your age: " << std::endl;
 
-    int age;
+    //int age = 0;
 
-    std::cin >> age;
+    /*std::cin >> age;
     if(!(std::cin >> age)){
         std::cout << "You did not enter a valid number. Please try again.";
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
-    std::cout << _validation_::isAdult(age);
-    std::cout <<_validation_::isSenior(age);
+    }*/
+
+    std::cout <<"You are" << "8" << "years old" << std::endl;
+
 }
