1d42a8261e07afbd5012318fa258bd7034ebfea2
paminalessle <paminalessle@gmail.com>
2026-04-27 17:42:17 +0000

    Lab_3 finihed section 2

--- courses/coding-in-C++/Solutions/Lab_3.cpp
+++ courses/coding-in-C++/Solutions/Lab_3.cpp
@@ -1,36 +1,93 @@
 #include <iostream>
 #include <string.h>
 
+
 class User {
     protected: 
         std::string name;
         int id;
     public:
+        /*
+        @brief Constructs an object of class User
+
+        Initializes the name and id of the User
+
+        @param[1] name  The name of the user
+        @param[2] id  The id of the user
+        */
         User(std::string name, int id) : name(name), id(id){
-            std::cout << "Constructor od User" << std::endl;
+            std::cout << "Constructor of User" << std::endl;
         }
-        void printInfo() {
-            std::cout << name << std::endl;
-            std::cout << id << std::endl;
-        }
+        /*
+        @brief Prints the information of the User
+        */
+        void printInfo();
 };
+
+void User::printInfo(){
+    std::cout << "Name: " << this->name << std::endl;
+    std::cout << "ID: " << this->id << std::endl;
+}
 
 class Student : public User {
     private: 
         int matNr;
     public:
-        Student(std::string name, int id, int matNr) : User(name, id), matNr(matNr){
-            std::cout << "COnstructor of Student" <<std::endl;
-        }
-        int set_points(int additional_points){
-            this->points += additional_points;
-        }
-        void printRole() {
+        /*
+        @brief Constructs a Student object
 
+        @param[1] name  The name of the student
+        @param[2] id  The id of the student
+        @param[3] matNr The matriculation number of the student
+        */
+        Student(std::string name, int id, int matNr) : User(name, id), matNr(matNr){
+            std::cout << "Constructor of Student" <<std::endl;
         }
+        
+        void printRole();
     
 };
+void Student::printRole() {
+            std::cout << "Role: Student" << std::endl;
+            std::cout << "Name: " << this->name << std::endl;
+            std::cout << "ID: " << this->id << std::endl;
+            std::cout << "Matriculation number: " << this->matNr << std::endl;
+}
 
 class Instructor : public User {
+    private: 
+        int matNr;
+    public:
+        /*
+        @brief Constructs an Instructor object
 
+        @param[1] name  The name of the instructor
+        @param[2] id  The id of the instructor
+        @param[3] matNr The matriculation number of the instructor
+        */
+        Instructor(std::string name, int id, int matNr) : User(name,id), matNr(matNr) {
+            std::cout << "Constructor of Instructor" << std:: endl;
+        }
+        
+        void printRole();
 };
+
+void Instructor::printRole(){
+            std::cout << "Role: Instructor" << std::endl;
+            std::cout << "Name: " << this->name << std::endl;
+            std::cout << "ID: " << this->id << std::endl;
+            std::cout << "Matriculation number: " << this->matNr << std::endl;
+}
+
+int main(){
+    Student John("John Boyle", 24, 18374);
+    Instructor Miles("Miles Smith", 18, 39317);
+
+    John.printInfo();
+    John.printRole();
+
+    Miles.printInfo();
+    Miles.printRole();
+
+    return 0;
+}
